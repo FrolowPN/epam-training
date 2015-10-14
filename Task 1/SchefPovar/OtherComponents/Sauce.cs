b86166561eb7a3ea06425df15d3433c41f96f95b
FrolowPN <frolowpn@mail.ru>
2015-10-14 22:42:56 +0400

    grouping of classes and interfaces in the folder

---
+++ Task 1/SchefPovar/OtherComponents/Sauce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchefPovar
+{
+    public class Sauce: IComponent
+    {
+        public string Name { get; set; }
+        public int CaloricContent { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Заправка - {0} \tКалорийность - {1} ккал.", Name, CaloricContent);
+        }
+    }
+}
---
+++ Task 1/SchefPovar/Vegetables/Pepper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchefPovar
+{
+    public class Pepper : Vegetables, IComponent
+    {
+        public string Color { get; set; }
+
+        public new void Print()
+        {
+            Console.WriteLine("- {0} \tВес - {1} г. \tКалорийность - {2} ккал. \tЦвет - {3}", Name, Weight, CaloricContent, Color);
+        }
+    }
+}
