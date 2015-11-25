d92d7cfaaae2249ce18a47e204c89e5b94672fa6
FrolowPN <frolowpn@mail.ru>
2015-11-25 20:42:51 +0400

    add class "ClientManager" and method AddClient;

---
+++ Task 4/ParserFiles/BL/ClientManager.cs
@@ -0,0 +1,21 @@
+﻿using DA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ClientManager
+    {
+        public void AddClient(string clientName)
+        {
+            BaseContext ctx = new BaseContext();
+            Client client = new Client() { Name = clientName };
+            ctx.Clients.Add(client);
+            ctx.SaveChanges();
+        }
+
+    }
+}
