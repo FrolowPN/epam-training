cd58e50c1469d1cc94e5b35340ac8960dedc4181
FrolowPN <frolowpn@mail.ru>
2015-11-25 20:55:40 +0400

    add method DeleteClient in class ClientManager

--- Task 4/ParserFiles/BL/ClientManager.cs
+++ Task 4/ParserFiles/BL/ClientManager.cs
@@ -22,6 +22,15 @@ namespace BL
             int notExistClient =0;
             return ctx.Clients.Where(x => x.Name == clientName).Count() != notExistClient;    
         }
+        public void DeleteClient(string clientName)
+        {
+            BaseContext ctx = new BaseContext();
+            if (ExistClient(clientName))
+            {
+                ctx.Clients.Remove(ctx.Clients.Where(x => x.Name == clientName).FirstOrDefault());
+                ctx.SaveChanges();
+            }
+        }
 
     }
 }
