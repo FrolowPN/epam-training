b5b4fe4312a8053220fc048d32fa2424b1ac7c6a
FrolowPN <frolowpn@mail.ru>
2015-11-25 21:12:32 +0400

    add methodes RenameClient and GetClient in class ClientManager

--- Task 4/ParserFiles/BL/ClientManager.cs
+++ Task 4/ParserFiles/BL/ClientManager.cs
@@ -19,17 +19,39 @@ namespace BL
         public bool ExistClient(string clientName)
         {
             BaseContext ctx = new BaseContext();
-            int notExistClient =0;
-            return ctx.Clients.Where(x => x.Name == clientName).Count() != notExistClient;    
+            int notExistClient = 0;
+            return ctx.Clients.Where(x => x.Name == clientName).Count() != notExistClient;
         }
         public void DeleteClient(string clientName)
         {
             BaseContext ctx = new BaseContext();
             if (ExistClient(clientName))
             {
-                ctx.Clients.Remove(ctx.Clients.Where(x => x.Name == clientName).FirstOrDefault());
+                ctx.Clients.Remove(GetClient(clientName));
                 ctx.SaveChanges();
             }
+        }
+        public Client GetClient(string nameClient)
+        {
+            BaseContext ctx = new BaseContext();
+            if (ExistClient(nameClient))
+            {
+                return ctx.Clients.Where(x => x.Name == nameClient).FirstOrDefault();
+            }
+            else
+            {
+                Client client = new Client() { Id = 00, Name = "Клиент не найден" };
+                return client;
+            }
+        }
+        public void RenameClient(string oldNameClient, string newNameClient)
+        {
+            BaseContext ctx = new BaseContext();
+            if (ExistClient(oldNameClient))
+            {
+              GetClient(oldNameClient).Name = newNameClient;  
+            }
+            ctx.SaveChanges();
         }
 
     }
