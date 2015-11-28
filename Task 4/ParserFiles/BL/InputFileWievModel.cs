77ecdecca28707ded1af314cafe107295da7af7d
FrolowPN <frolowpn@mail.ru>
2015-11-28 17:16:52 +0400

    add class "InputFileWievModel"

---
+++ Task 4/ParserFiles/BL/InputFileWievModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class InputFileWievModel
+    {
+        public DateTime DateFile { get; set; }
+        public string NameManager { get; set; }
+    }
+}
