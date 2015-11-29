da94dd7897d3c41f7da7acf422c1d554a433237c
FrolowPN <frolowpn@mail.ru>
2015-11-29 21:33:14 +0400

    add FileSystemWatcher

--- Task 4/ParserFiles/Program.cs
+++ Task 4/ParserFiles/Program.cs
@@ -13,6 +13,20 @@ namespace ParserFiles
     {
         static void Main(string[] args)
         {
+             string pathInput = ConfigurationManager.AppSettings["InputFolder"];
+            FileSystemWatcher watcher = new FileSystemWatcher(pathInput);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Press Enter to quit");
+            Console.ReadLine();
+            
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
             string pathInput = ConfigurationManager.AppSettings["InputFolder"];
             string pathOutput = ConfigurationManager.AppSettings["OutputFolder"];
             FileManager fileManager = new FileManager();
@@ -23,13 +37,12 @@ namespace ParserFiles
             InputFileManager inputFileManager = new InputFileManager();
             foreach (var item in result)
             {
-               var parseNameFile = parser.ParseNameFile(fileManager.GetNameFile(item));
-               var parseFile = parser.ParseFile(item);
-               inputFileManager.AddInputFile(parseNameFile, parseFile);
-               fileManager.MoveFile(item, pathOutput);
-               Console.WriteLine(fileManager.GetNameFile(item)+" - обработан");
+                var parseNameFile = parser.ParseNameFile(fileManager.GetNameFile(item));
+                var parseFile = parser.ParseFile(item);
+                inputFileManager.AddInputFile(parseNameFile, parseFile);
+                fileManager.MoveFile(item, pathOutput);
+                Console.WriteLine(fileManager.GetNameFile(item) + " - обработан");
             }
-            Console.ReadKey();
         }
     }
 }
